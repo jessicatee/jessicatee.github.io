4ece5eafb07a535a90653d5d1ea53b4233a0fd6d
Jess Taylor <jessicatee84@gmail.com>
2018-03-18 15:07:26 +0000

    Commented out js issue

--- js/scripts.js
+++ js/scripts.js
@@ -32,15 +32,7 @@ $('a[href*="#"]')
         $('html, body').animate({
           scrollTop: target.offset().top
         }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          // var $target = $(target);
-          // $target.focus();
-          // if ($target.is(":focus")) { // Checking if the target was focused
-          //   return false;
-          // } else {
-          //   $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          //   $target.focus(); // Set focus again
+          
           // };
         });
       }
